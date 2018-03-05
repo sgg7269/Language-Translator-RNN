add668e0a884e8c906a40b864e1ff47b7a7df7ca
Scott Gaydos <scott.gaydos@yum.com>
2018-03-04 20:51:34 -0600

    fucking ipynb get outta here

---
+++ rnn-test.py
@@ -0,0 +1,59 @@
+import time
+
+def get_accuracy(target, logits):
+    """
+    Calculate accuracy
+    """
+    max_seq = max(target.shape[1], logits.shape[1])
+    if max_seq - target.shape[1]:
+        target = np.pad(
+            target,
+            [(0,0),(0,max_seq - target.shape[1])],
+            'constant')
+    if max_seq - logits.shape[1]:
+        logits = np.pad(
+            logits,
+            [(0,0),(0,max_seq - logits.shape[1]), (0,0)],
+            'constant')
+
+    return np.mean(np.equal(target, np.argmax(logits, 2)))
+
+train_source = source_int_text[batch_size:]
+train_target = target_int_text[batch_size:]
+
+valid_source = helper.pad_sentence_batch(source_int_text[:batch_size])
+valid_target = helper.pad_sentence_batch(target_int_text[:batch_size])
+
+with tf.Session(graph=train_graph) as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for epoch_i in range(epochs):
+        for batch_i, (source_batch, target_batch) in enumerate(
+                helper.batch_data(train_source, train_target, batch_size)):
+            start_time = time.time()
+            
+            _, loss = sess.run(
+                [train_op, cost],
+                {input_data: source_batch,
+                 targets: target_batch,
+                 lr: learning_rate,
+                 sequence_length: target_batch.shape[1],
+                 keep_prob: keep_probability})
+            
+            batch_train_logits = sess.run(
+                inference_logits,
+                {input_data: source_batch, keep_prob: 1.0})
+            batch_valid_logits = sess.run(
+                inference_logits,
+                {input_data: valid_source, keep_prob: 1.0})
+                
+            train_acc = get_accuracy(target_batch, batch_train_logits)
+            valid_acc = get_accuracy(np.array(valid_target), batch_valid_logits)
+            end_time = time.time()
+            print('Epoch {:>3} Batch {:>4}/{} - Train Accuracy: {:>6.3f}, Validation Accuracy: {:>6.3f}, Loss: {:>6.3f}'
+                  .format(epoch_i, batch_i, len(source_int_text) // batch_size, train_acc, valid_acc, loss))
+
+    # Save Model
+    saver = tf.train.Saver()
+    saver.save(sess, save_path)
+    print('Model Trained and Saved')
